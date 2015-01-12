23850d18e5939b2a8226d617bfde820b8f8443f1
Vladimir <volkvid@yandex.ru>
2015-01-12 19:15:51 +0300

    formatting + improvements (faster now)

--- main.py
+++ main.py
@@ -13,70 +13,65 @@ def main():
     try:
         f = open('_input.txt', 'r')
     except:
-        print 'No input file'
+        print 'No _input.txt file'
         quit()
 
     inp = f.readline()
     while inp != '':
         # prepare template
         p = parse(inp) #group link
-        g = open('output/' + p + '.html', 'w')
-        g.write('<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>'+p+\
-                '</title>\n</head>\n<body>\n'+\
-                '<h3 align="center"><a href="'+inp+'" target="_blank">'+p+'</a></h3>'+\
+        g = open('output/{0}.html'.format(p), 'w')
+        g.write('<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>{0}</title>\n</head>\n<body>\n'+\
+                '<h3 align="center"><a href="{1}" target="_blank">{0}</a></h3>'+\
                 '<table width="50%" border="1" align="center"><tr align="center">'+\
-                '<td width="15%">likes</td><td width="15%">comm</td><td width="15%">reposts</td><td>link</td></tr>')
-        g.close
-        print str(groupNum)+'. group name: ' + p
+                '<td width="15%">likes</td><td width="15%">comm</td><td width="15%">reposts</td><td>link</td></tr>'.format(p, inp))
+        g.close()
+        print '{0}. group name: {1}'.format(groupNum, p)
         groupNum += 1
 
         # prepare and tryout 
         ret = getA(p, 0, 1)
         count = ret['response']['count']
-        print 'num to parse:', str(count), '\n'
+        print 'num to parse:{0}\n'.format(count)
         offset = count//100 + 1
         output = []
+        append = output.append
         for i in range(0, offset):
             posts = getA(p, i*100, 100)
             for post in posts['response']['items']:
                 #print post, '\n'
                 try:
-                    link = 'vk.com/'+p+'?w=wall'+str(post['from_id']) + '_' + str(post['id'])
-
+                    link = 'vk.com/{0}?w=wall-{1}_{2}'.format(p, post['owner_id'], post['id'])
                     comm = post['comments']['count']
                     like = post['likes']['count']
                     repo = post['reposts']['count']
                     
-                    output.append({'link': link, 'comm': comm, 'like': like, 'repo': repo})
+                    append([like, link, comm, repo])
                 except BaseException as ex:
                     print ex
                 glob += 1
 
         # sort by likes 
-        a = [[o['like'], o['link'], o['comm'], o['repo']] for o in output] 
         a.sort(reverse=True)
 
         # the result representing
-        g = open('output/' + p + '.html', 'a')
+        g = open('output/{0}.html'.format(p), 'a')
         for i in range(0, len(a)):
-            g.write('\n<tr align="center">\n<td>'+ str(a[i][0]) +\
-                    '</td>\n<td>'+str(a[i][2])+\
-                    '</td>\n<td>'+str(a[i][3])+\
-                    '</td>\n<td><a href="https://'+a[i][1]+\
-                    '" target="_blank">Check it' +\
-                    '</a></td>\n</tr>')            
+            g.write('\n<tr align="center">\n<td>{0}</td>\n<td>{1}</td>\n<td>{2}'+\
+                    '</td>\n<td><a href="https://{3}" target="_blank">Check it' +\
+                    '</a></td>\n</tr>'.format(a[i][0], a[i][2], a[i][3], a[i][1]))            
         g.write('</table>\n<br><div align="center"><a href="https://github.com/d3QUone/vk-analytics2" '+\
                 'target="_blank">Follow me on GitHub!</a></div><br>\n</body>\n</html>')
         g.close
         inp = f.readline()
         
     f.close
-    print '\nDone.'
     t = datetime.now() - t
-    print '\nWorked = ' + str(t) + '\n\ntotal posts = ' + str(glob)
+    print '\nDone.'
+    print '\nWorked = {0}\n\ntotal posts = {1}'.format(t, glob)
     stat = open('_stat.txt', 'w')
-    stat.write('worked = ' + str(t) + '\n\ntotal posts = ' + str(glob))
-    stat.close
+    stat.write('worked = {0}\n\ntotal posts = {1}'.format(t, glob))
+    stat.close()
 
 
 def parse(st):
@@ -90,7 +85,6 @@ def parse(st):
             start = 2
         elif start == 2: 
             out += st[i]
-
     end = len(out)-1
     if out[end] == '\n':
         out = out[:end]
@@ -98,8 +92,7 @@ def parse(st):
 
 
 def getA(dom, off=0, cou=100):
-    response = urllib.urlopen('https://api.vk.com/method/wall.get?domain='+dom+\
-                              '&offset='+str(off)+'&count=' + str(cou) + '&v=5.26')
+    response = urllib.urlopen('https://api.vk.com/method/wall.get?domain={0}&offset={1}&count={2}&v=5.26'.format(dom, off, cou))
     return json.load(response)
 
                               
