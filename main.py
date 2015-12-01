4eab1caecbbccff0aa04ebb1df1c5ffe2049c76c
Vladimir <volkvid@yandex.ru>
2015-12-02 00:18:02 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
 # -*- coding: cp1251 -*-
+import os
 import sys
 from datetime import datetime
+
 import requests
 
+
 FILENAME = "_input.txt"
+RES_DIR = "output"
 
 def main():
-    # for stats
     t = datetime.now()
     glob = 0
     groupNum = 1
-
-    # check input
-    try:
-        f = open('_input.txt', 'r')
-    except IOError:
+    if not os.path.isfile(FILENAME):
         print 'No _input.txt file'
         sys.exit()
-
+    if not os.path.isdir(RES_DIR):
+        os.mkdir(RES_DIR)
+    f = open(FILENAME, 'r')
     inp = f.readline()
     while inp != '':
         # prepare template
-        p = parse(inp) #group link
-        g = open('output/{0}.html'.format(p), 'w')
-        g.write('<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>{0}</title>\n</head>\n<body>\n'+\
-                '<h3 align="center"><a href="{1}" target="_blank">{0}</a></h3>'+\
-                '<table width="50%" border="1" align="center"><tr align="center">'+\
+        p = parse(inp)
+        g = open(os.path.join(RES_DIR, "{0}.html".format(p)), "w")
+        g.write('<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>{0}</title>\n</head>\n<body>\n'
+                '<h3 align="center"><a href="{1}" target="_blank">{0}</a></h3>'
+                '<table width="50%" border="1" align="center"><tr align="center">'
                 '<td width="15%">likes</td><td width="15%">comm</td><td width="15%">reposts</td><td>link</td></tr>'.format(p, inp))
         g.close()
         print '{0}. group name: {1}'.format(groupNum, p)
         groupNum += 1
 
         # prepare and tryout 
-        ret = getInfo(p, 0, 1)
+        ret = request_data(p, 0, 1)
         count = ret['response']['count']
         print 'num to parse: {0}\n'.format(count)
         offset = count//100 + 1
         output = []
         append = output.append
         for i in range(0, offset):
-            posts = getInfo(p, i*100, 100)
+            posts = request_data(p, i*100, 100)
             for post in posts['response']['items']:
-                #print post, '\n'
                 try:
                     link = 'vk.com/{0}?w=wall-{1}_{2}'.format(p, post['owner_id'], post['id'])
                     comm = post['comments']['count']
@@ -49,20 +49,18 @@ def main():
                     repo = post['reposts']['count']
                     
                     append([like, link, comm, repo])
-                except BaseException as ex:
-                    print ex
+                except Exception as ex:
+                    print "Post-parsing exception: {0}".format(ex)
                 glob += 1
-
-        # sort by likes 
+        # sort by likes
         output.sort(reverse=True)
-
         # the result representing
-        g = open('output/{0}.html'.format(p), 'a')
-        for i in range(0, len(a)):
-            g.write('\n<tr align="center">\n<td>{0}</td>\n<td>{1}</td>\n<td>{2}'+\
-                    '</td>\n<td><a href="https://{3}" target="_blank">Check it' +\
-                    '</a></td>\n</tr>'.format(a[i][0], a[i][2], a[i][3], a[i][1]))            
-        g.write('</table>\n<br><div align="center"><a href="https://github.com/d3QUone/vk-analytics2" '+\
+        g = open(os.path.join(RES_DIR, "{0}.html".format(p)), "a")
+        for i in range(0, len(output)):
+            g.write('\n<tr align="center">\n<td>{0}</td>\n<td>{1}</td>\n<td>{2}'
+                    '</td>\n<td><a href="https://{3}" target="_blank">Check it'
+                    '</a></td>\n</tr>'.format(output[i][0], output[i][2], output[i][3], output[i][1]))
+        g.write('</table>\n<br><div align="center"><a href="https://github.com/d3QUone/vk-analytics2" '
                 'target="_blank">Follow me on GitHub!</a></div><br>\n</body>\n</html>')
         g.close()
         inp = f.readline()
@@ -71,13 +69,12 @@ def main():
     t = datetime.now() - t
     print '\nDone.'
     print '\nWorked = {0}\n\ntotal posts = {1}'.format(t, glob)
-    stat = open('_stat.txt', 'w')
-    stat.write('worked = {0}\n\ntotal posts = {1}'.format(t, glob))
-    stat.close()
+    with open('_stat.txt', 'w') as stat:
+        stat.write('worked = {0}\n\ntotal posts = {1}'.format(t, glob))
 
 
 def parse(st):
-    i = start = 0
+    start = 0
     end = len(st)
     out = ''
     for i in range(end):
@@ -93,15 +90,15 @@ def parse(st):
     return out
 
 
-def getInfo(domain, offset=0, count=100):
+def request_data(domain, offset=0, count=100):
     params = {
         "domain": domain,
         "offset": offset,
         "count": count,
-        "v": "5.26"
+        "v": "5.27"
     }
-    r = requests.get("https://api.vk.com/method/wall.get", params=params)
-    return r.json()               
+    r = requests.get("https://api.vk.com/method/wall.get", params=params, timeout=5.0)
+    return r.json()
 
 
 if __name__ == "__main__":
--- sort.py
+++ sort.py
@@ -1,41 +1,39 @@
 import sys
 
-from main import FILENAME, getInfo
+from main import FILENAME, request_data
+
 
 def sort_input():  
     try:
-        f = open(FILENAME, "r")
-        data = f.read().split('\n')
-        f.close()
-        
-        # network here..
+        with open(FILENAME, "r") as f:
+            data = f.read().split("\n")
         buf = []
         append = buf.append
         for link in data:
+            if link == "":
+                continue
             try:
-                L = link.replace("\n", "").split("/")
+                L = link.split("/")
                 dom = L[len(L)-1]
-                ret = getInfo(dom, 0, 1)
-                count = ret["response"]["count"]
-                append({"link": link, "count": count})
-            except BaseException as ex:
-                print ex
-
-        # sort that
+                ret = request_data(dom, 0, 1)
+                if "response" in ret and "count" in ret["response"]:
+                    count = ret["response"]["count"]
+                    append({"link": link, "count": count})
+                else:
+                    print "Bad response: {0}".format(ret)
+            except Exception as e:
+                print "Sorting exceprion: {0}".format(repr(e))
         output = [[o["count"], o["link"]] for o in buf]
         output.sort(reverse=True)
-        
-        # save now
-        f = open(FILENAME, "w+")
+        f = open(FILENAME, "w")
         for item in output:
-            f.write(item[1] + '\n')
+            f.write("{0}\n".format(item[1]))
         f.close()
         print "Done"
     except IOError:
         print "No input file"
         sys.exit()
-    
-        
+
 
 if __name__ == "__main__":
     sort_input()
